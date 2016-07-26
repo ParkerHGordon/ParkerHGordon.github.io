61bb1896ec0ee826d1f1c66abda4f0afe7de0ea9
Parker Gordon <parker.h.gordon@gmail.com>
2016-07-26 17:57:45 +0000

    finish painting, start login

--- Projects/bobRoss/paintingTime.js
+++ Projects/bobRoss/paintingTime.js
@@ -1,27 +1,65 @@
-var canvas = document.getElementById("paintingCanvas"); 
-var ctx = canvas.getContext("2d");
-var mouseX, mouseY;
-var paintMethod = "fillRect";
-canvas.addEventListener('mousemove', paint, true);
-function getMouseCoordinates(canvas, e) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
+
+// used this example tutorial to help me out when I was stuck: http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/#demo-simple
+
+
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); 
+var paint = false;
+var mouseX;
+var mouseY;
+var posX = new Array();
+var posY = new Array();
+var posDrag = new Array();
+var rect = canvas.getBoundingClientRect();
+
+function addPos (x, y, dragging) {
+    posX.push(x);
+    posY.push(y);
+    posDrag.push(dragging);
 }
 
-function paint (evt) {
-    var pos = getMouseCoordinates(canvas, evt);
-    ctx.fillStyle = "#6677ee";
-    ctx.imageSmoothingEnabled = false;
-    switch (paintMethod) {
-            case "fillRect":
-                ctx.fillRect(pos.x, pos.y, 4, 4);
-                break;
-            case "somethingElse":
-                
-                break;
-
+function redraw () {
+    ctx.strokeStyle = '#6677ee';
+    ctx.lineJoin = 'round';
+    ctx.lineWidth = 5;
+    for (var i = 0; i < posX.length; i++) {
+        ctx.beginPath();
+        if(posDrag[i] && i){
+            ctx.moveTo(posX[i-1], posY[i-1]);
+        }else{
+            ctx.moveTo(posX[i], posY[i]);
+        }
+        ctx.lineTo(posX[i], posY[i]);
+        ctx.closePath();
+        ctx.stroke();
     }
+    
 }
+$('#canvas').on('mousedown', function (e) {
+    
+    mouseX = e.clientX - rect.left;
+    mouseY = e.clientY - rect.top;
+    
+    paint = true;
+    addPos(mouseX, mouseY);
+    redraw();
+});
+$('#canvas').on('mousemove', function (e) {
+    if (paint === true) {
+        mouseX = e.clientX - rect.left;
+        mouseY = e.clientY - rect.top;
+        addPos(mouseX, mouseY, true);
+        redraw();
+    }
+    
+});
+
+$('#canvas').on('mouseup', function (e) {
+    paint = false;
+    
+});
+
+$('#canvas').on('mouseleave', function (e) {
+    paint = false;
+});
